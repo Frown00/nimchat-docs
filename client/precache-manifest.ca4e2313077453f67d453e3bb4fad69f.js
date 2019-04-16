3b0b732514eff7f68fc11bccd3e58dcf84c318a4
Fabian Kuriata <fabian.kuriata.dev@gmail.com>
2019-04-16 21:30:28 +0200

    add client doc

---
+++ client/precache-manifest.ca4e2313077453f67d453e3bb4fad69f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "25ef280441ad6d3a5ccf89960b4e95eb",
+    "url": "static/media/spinner.25ef2804.gif"
+  },
+  {
+    "revision": "17ae5abff1c62b621e12264f223e2ab1",
+    "url": "static/media/logo.17ae5abf.PNG"
+  },
+  {
+    "revision": "e71f5fedf79e25b0020c",
+    "url": "static/css/main.62ccb507.chunk.css"
+  },
+  {
+    "revision": "bf7ba4546fb593b358dbd8c4eb4ae0c2",
+    "url": "index.html"
+  },
+  {
+    "revision": "e71f5fedf79e25b0020c",
+    "url": "build/main.e71f5fed.js"
+  },
+  {
+    "revision": "feb19782ca779508500c",
+    "url": "build/bundle.feb19782.js"
+  },
+  {
+    "revision": "6be0e19b1b909afa5d04",
+    "url": "build/2.6be0e19b.js"
+  }
+];
